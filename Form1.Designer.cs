92bb9f62f460cef787e814cb640effed25a97682
akoot <akoot@akoot.me>
2023-03-02 12:35:13 -0500

    Changed up the design a bit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,76 +29,58 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.dailyRadioButton = new System.Windows.Forms.RadioButton();
             this.weeklyRadioButton = new System.Windows.Forms.RadioButton();
             this.monthlyRadioButton = new System.Windows.Forms.RadioButton();
             this.loadStockButton = new System.Windows.Forms.Button();
-            this.loadProgress = new System.Windows.Forms.ProgressBar();
             this.startDate = new System.Windows.Forms.DateTimePicker();
             this.endDate = new System.Windows.Forms.DateTimePicker();
-            this.daListBoxe = new System.Windows.Forms.ListBox();
-            this.candleStickBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.leChart = new System.Windows.Forms.DataGridView();
-            this.candleStickBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.daListBoxe = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.openDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.highDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lowDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.closeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.volumeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).BeginInit();
+            this.candleStickBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.candleStickBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.leChart)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(101, 60);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Select a stock";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(80, 362);
+            this.label3.Location = new System.Drawing.Point(5, 75);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.Size = new System.Drawing.Size(61, 16);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Pick an end date";
+            this.label3.Text = "End date";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(80, 281);
+            this.label4.Location = new System.Drawing.Point(2, 18);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(102, 16);
+            this.label4.Size = new System.Drawing.Size(64, 16);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Pick a start date";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(569, 44);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(62, 16);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Progress";
+            this.label4.Text = "Start date";
             // 
             // dailyRadioButton
             // 
             this.dailyRadioButton.AutoSize = true;
-            this.dailyRadioButton.Location = new System.Drawing.Point(111, 137);
+            this.dailyRadioButton.Location = new System.Drawing.Point(5, 20);
             this.dailyRadioButton.Margin = new System.Windows.Forms.Padding(2);
             this.dailyRadioButton.Name = "dailyRadioButton";
             this.dailyRadioButton.Size = new System.Drawing.Size(59, 20);
@@ -111,7 +93,7 @@
             // weeklyRadioButton
             // 
             this.weeklyRadioButton.AutoSize = true;
-            this.weeklyRadioButton.Location = new System.Drawing.Point(111, 170);
+            this.weeklyRadioButton.Location = new System.Drawing.Point(68, 20);
             this.weeklyRadioButton.Margin = new System.Windows.Forms.Padding(2);
             this.weeklyRadioButton.Name = "weeklyRadioButton";
             this.weeklyRadioButton.Size = new System.Drawing.Size(74, 20);
@@ -124,7 +106,7 @@
             // monthlyRadioButton
             // 
             this.monthlyRadioButton.AutoSize = true;
-            this.monthlyRadioButton.Location = new System.Drawing.Point(111, 198);
+            this.monthlyRadioButton.Location = new System.Drawing.Point(146, 20);
             this.monthlyRadioButton.Margin = new System.Windows.Forms.Padding(2);
             this.monthlyRadioButton.Name = "monthlyRadioButton";
             this.monthlyRadioButton.Size = new System.Drawing.Size(74, 20);
@@ -136,7 +118,7 @@
             // 
             // loadStockButton
             // 
-            this.loadStockButton.Location = new System.Drawing.Point(459, 61);
+            this.loadStockButton.Location = new System.Drawing.Point(666, 336);
             this.loadStockButton.Margin = new System.Windows.Forms.Padding(2);
             this.loadStockButton.Name = "loadStockButton";
             this.loadStockButton.Size = new System.Drawing.Size(109, 29);
@@ -145,17 +127,9 @@
             this.loadStockButton.UseVisualStyleBackColor = true;
             this.loadStockButton.Click += new System.EventHandler(this.loadStockButton_Click);
             // 
-            // loadProgress
-            // 
-            this.loadProgress.Location = new System.Drawing.Point(573, 61);
-            this.loadProgress.Margin = new System.Windows.Forms.Padding(2);
-            this.loadProgress.Name = "loadProgress";
-            this.loadProgress.Size = new System.Drawing.Size(497, 29);
-            this.loadProgress.TabIndex = 10;
-            // 
             // startDate
             // 
-            this.startDate.Location = new System.Drawing.Point(83, 304);
+            this.startDate.Location = new System.Drawing.Point(5, 41);
             this.startDate.Margin = new System.Windows.Forms.Padding(2);
             this.startDate.Name = "startDate";
             this.startDate.Size = new System.Drawing.Size(247, 22);
@@ -163,24 +137,11 @@
             // 
             // endDate
             // 
-            this.endDate.Location = new System.Drawing.Point(83, 380);
+            this.endDate.Location = new System.Drawing.Point(8, 93);
             this.endDate.Margin = new System.Windows.Forms.Padding(2);
             this.endDate.Name = "endDate";
             this.endDate.Size = new System.Drawing.Size(247, 22);
             this.endDate.TabIndex = 12;
-            // 
-            // daListBoxe
-            // 
-            this.daListBoxe.FormattingEnabled = true;
-            this.daListBoxe.ItemHeight = 16;
-            this.daListBoxe.Location = new System.Drawing.Point(239, 61);
-            this.daListBoxe.Name = "daListBoxe";
-            this.daListBoxe.Size = new System.Drawing.Size(120, 84);
-            this.daListBoxe.TabIndex = 15;
-            // 
-            // candleStickBindingSource
-            // 
-            this.candleStickBindingSource.DataSource = typeof(Stock_Ticker.CandleStick);
             // 
             // leChart
             // 
@@ -193,17 +154,58 @@
             this.lowDataGridViewTextBoxColumn,
             this.closeDataGridViewTextBoxColumn,
             this.volumeDataGridViewTextBoxColumn});
+            this.leChart.Cursor = System.Windows.Forms.Cursors.Default;
             this.leChart.DataSource = this.candleStickBindingSource1;
-            this.leChart.Location = new System.Drawing.Point(414, 116);
+            this.leChart.Location = new System.Drawing.Point(12, 15);
             this.leChart.Name = "leChart";
             this.leChart.RowHeadersWidth = 51;
             this.leChart.RowTemplate.Height = 24;
-            this.leChart.Size = new System.Drawing.Size(692, 380);
+            this.leChart.Size = new System.Drawing.Size(646, 376);
             this.leChart.TabIndex = 16;
             // 
-            // candleStickBindingSource1
+            // groupBox1
             // 
-            this.candleStickBindingSource1.DataSource = typeof(Stock_Ticker.CandleStick);
+            this.groupBox1.Controls.Add(this.dailyRadioButton);
+            this.groupBox1.Controls.Add(this.weeklyRadioButton);
+            this.groupBox1.Controls.Add(this.monthlyRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(666, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(263, 56);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.startDate);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.endDate);
+            this.groupBox2.Location = new System.Drawing.Point(666, 204);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(263, 127);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            // 
+            // daListBoxe
+            // 
+            this.daListBoxe.FormattingEnabled = true;
+            this.daListBoxe.ItemHeight = 16;
+            this.daListBoxe.Location = new System.Drawing.Point(666, 52);
+            this.daListBoxe.Name = "daListBoxe";
+            this.daListBoxe.Size = new System.Drawing.Size(120, 84);
+            this.daListBoxe.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stock Symbol";
             // 
             // dateDataGridViewTextBoxColumn
             // 
@@ -253,50 +255,52 @@
             this.volumeDataGridViewTextBoxColumn.Name = "volumeDataGridViewTextBoxColumn";
             this.volumeDataGridViewTextBoxColumn.Width = 125;
             // 
+            // candleStickBindingSource1
+            // 
+            this.candleStickBindingSource1.DataSource = typeof(Stock_Ticker.CandleStick);
+            // 
+            // candleStickBindingSource
+            // 
+            this.candleStickBindingSource.DataSource = typeof(Stock_Ticker.CandleStick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1118, 536);
-            this.Controls.Add(this.leChart);
-            this.Controls.Add(this.daListBoxe);
-            this.Controls.Add(this.endDate);
-            this.Controls.Add(this.startDate);
-            this.Controls.Add(this.loadProgress);
+            this.ClientSize = new System.Drawing.Size(940, 408);
             this.Controls.Add(this.loadStockButton);
-            this.Controls.Add(this.monthlyRadioButton);
-            this.Controls.Add(this.weeklyRadioButton);
-            this.Controls.Add(this.dailyRadioButton);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.daListBoxe);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.leChart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3);
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.leChart)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.RadioButton dailyRadioButton;
         private System.Windows.Forms.RadioButton weeklyRadioButton;
         private System.Windows.Forms.RadioButton monthlyRadioButton;
         private System.Windows.Forms.Button loadStockButton;
-        private System.Windows.Forms.ProgressBar loadProgress;
         private System.Windows.Forms.DateTimePicker startDate;
         private System.Windows.Forms.DateTimePicker endDate;
-        private System.Windows.Forms.ListBox daListBoxe;
         private System.Windows.Forms.BindingSource candleStickBindingSource;
         private System.Windows.Forms.DataGridView leChart;
         private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
@@ -306,6 +310,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn closeDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn volumeDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource candleStickBindingSource1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox daListBoxe;
     }
 }
 
